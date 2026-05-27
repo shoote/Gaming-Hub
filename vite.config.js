ecbbd352fdcfa04e10e1b4eb6d74d1c29e8b1cc1
Harsh Gahlot <gahlotharsh19@gmail.com>
2026-05-27 20:39:59 +0530

    gaming hub

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    cssCodeSplit: false // 👈 Add this line here to fix the Vercel error
+  }
 })
